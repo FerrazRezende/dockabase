2f9e944c3b0773952012e487a309d9da060b7ae5
Matheus Ferraz <mtsferraz.rezende@gmail.com>
2026-04-07 19:16:29 -0300

    fix: add CSRF token meta tag to layout
    - The fetch() for locale change needs CSRF token
    - Added <meta name="csrf-token" content="{{ csrf_token() }}"> to app.blade.php
    - This fixes the 419 Page Expired error when changing language

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -15,6 +15,10 @@ class LocaleController extends Controller
     {
         $locale = $request->input('locale');
 
+        // Debug: log what we receive
+        \Log::info('LocaleController::set - Received locale: ' . $locale);
+        \Log::info('LocaleController::set - Request data: ' . json_encode($request->all()));
+
         // Validate locale
         if (!in_array($locale, ['pt', 'en', 'es'])) {
             $locale = 'pt';
@@ -22,6 +26,9 @@ class LocaleController extends Controller
 
         // Store in session
         session()->put('locale', $locale);
+
+        // Debug: log session
+        \Log::info('LocaleController::set - Session locale set to: ' . session('locale'));
 
         // Set immediately for current request
         App::setLocale($locale);
